7da1b87738a0294c71bdfaeb05d79f8b32281a8f
Youngsun Kim <yskim041@gmail.com>
2020-11-19 23:21:07 -0800

    update script and readme

--- README.md
+++ README.md
@@ -1,28 +1,45 @@
-# naver_news_crawling
-Python을 이용한 네이버 뉴스 리스트 크롤링 프로그램 (naver news crawling)
+# Naver News Crawling
+Python을 이용한 네이버 뉴스 검색 결과 크롤링 프로그램
 
-------
- 사용 라이브러리  
--------------
-- BeautifulSoup
-- requests
-- pandas
-- datetime (현 시간을 이름으로 저장하기 위함)
-- re  (정규표현식)
+## 설치
+git 명령을 사용해 적당한 위치에 repo를 다운로드 합니다.
+```
+$ cd <YOUR_DIRECTORY>
+$ git clone https://github.com/yskim041/naver_news_crawling.git
+```
 
+프로그램에서 사용하는 라이브러리들을 설치해줍니다.
 
- 프로그램 소개 
- -------------
-- select 연산자 사용 (find x)
-- 크롤링 해오는 것 : 링크,제목,신문사,날짜,내용요약본
-- 날짜,내용요약본  -> 정제 작업 필요
-- 크롤링 결과 : 리스트 -> 딕셔너리 -> df -> 엑셀로 저장 
+#### pip 을 사용하는 경우
+```
+$ pip install -r requirements.txt
+```
 
-------
- 실행 
--------------
-![1](1.png)
+#### conda 를 사용하는 경우
+```
+$ conda install --file requirements.txt
+```
 
-![2](2.png)
+## 사용방법
+다운받은 repo가 저장된 폴더로 이동 후, `naver_news_crawling.py` 스크립트를 실행합니다.
+```
+$ cd <YOUR_DIRECTORY>
+$ cd naver_news_crawling
+$ python naver_news_crawling.py
+```
 
-![3](3.png)
+스크립트가 실행되면, 화면에 출력되는 안내에 따라 검색어, 페이지 수, 검색 방식, 검색 일자를 설정해줍니다. 기본 설정 값은 configs.json 에서 설정할 수 있습니다. 반복적으로 같은 방식의 검색을 수행할 경우, 아래 *기본 설정 변경* 파트를 참고하여 configs.json 파일을 적절하게 설정한 후 사용하시기 바랍니다.
+
+#### 결과 확인
+검색 결과는 스크립트가 실행된 경로 아래 `results` 라는 폴더 내에 저장됩니다. 또는, configs.json 내에 지정한 `output_directory` 안에 저장하도록 설정할 수 있습니다.
+
+#### 기본 설정 변경
+repo가 저장된 폴더 안에 있는 `configs.json` 파일을 수정하여 기본 설정을 변경할 수 있습니다. 변경 가능한 옵션은 다음과 같습니다.
+
+| Option                | Description                                                     |
+|-----------------------|-----------------------------------------------------------------|
+| `default_query`       | 기본 검색 키워드                                                |
+| `max_pages`           | 수집할 최대 페이지 수 (한 페이지당 10개의 기사가 포함됨)        |
+| `sort_type`           | 기사 검색 방식 (관련도순=0, 최신순=1, 오래된순=2)               |
+| `date_range`          | 검색 일자 범위 (오늘로부터 며칠 전 기사부터 검색할 것인지 설정) |
+| `output_directory`    | 검색 결과 파일을 저장할 경로                                    |
---
+++ naver_news_crawling.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+"""
+Naver에서 뉴스를 검색 후, 결과 리스트의 주요 내용을 엑셀로 저장하는 프로그램
+"""
+
+import os
+import sys
+import json
+import re
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+
+def validate_date_text(date_txt):
+    try:
+        datetime.strptime(date_txt, '%Y.%m.%d.')
+        return date_txt
+    except:
+        return None
+
+
+def contents_cleansing(contents):
+    # 컨텐츠 앞뒤의 불필요한 부분 제거
+    clean_contents = re.sub('<dl>.*?</a> </div> </dd> <dd>', '',
+                            str(contents)).strip()
+    clean_contents = re.sub('<ul class="relation_lst">.*?</dd>', '',
+                            clean_contents).strip()
+    clean_contents = re.sub('<.+?>', '', clean_contents).strip()
+    return clean_contents
+
+
+def crawler(max_pages, query, sort_type, start_date, end_date, output_dir):
+    title_text = []
+    link_text = []
+    source_text = []
+    date_text = []
+    contents_text = []
+    result = {}
+
+    s_from = start_date.replace(".", "")
+    e_to = end_date.replace(".", "")
+    page = 1
+    max_pages_t = (int(max_pages) - 1) * 10 + 1  # 11=2페이지, 21=3페이지, ...
+    data_frame = None
+
+    for page in range(1, max_pages_t + 1, 10):
+        url = "https://search.naver.com/search.naver?where=news&query=\"" + query + "\"&sort="+sort_type+"&ds=" + start_date + "&de=" + end_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
+        print(f'검색결과 수집 중... page {page // 10 + 1}')
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 제목과 링크주소 추출
+        atags = soup.find_all('a', class_='news_tit')
+        for atag in atags:
+            title_text.append(atag.text)    # 제목
+            link_text.append(atag['href'])  # 링크주소
+
+        # 매체명 추출
+        source_lists = soup.find_all('a', class_='info press')
+        for source_list in source_lists:
+            source_text.append(source_list.text)  # 매체명
+
+        # 날짜 추출
+        date_lists = soup.find_all('span', class_='info')
+        for date_list in date_lists:
+            valid_date = validate_date_text(date_list.text)
+            if valid_date is not None:
+                date_text.append(valid_date)
+
+        # 본문요약본
+        contents_lists = soup.find_all('a', class_='api_txt_lines')
+        for contents_list in contents_lists:
+            contents_text.append(contents_cleansing(contents_list))
+
+        # 다음 페이지가 존재하지 않을 경우 반복문 종료
+        if len(contents_lists) < 10:
+            break
+
+    # 결과를 DataFrame으로 변환
+    result = {"date": date_text, "title": title_text, "source": source_text, "contents": contents_text, "link": link_text}
+    data_frame = pd.DataFrame(result)
+
+    # 결과 저장
+    output_filepath = os.path.join(output_dir, '{}.xlsx'.format(datetime.now().strftime('%Y%m%d_%H%M%S')))
+    data_frame.to_excel(output_filepath, sheet_name='검색결과')
+    print(f'\n검색 완료: {output_filepath}')
+
+
+def main():
+    print('=' * 50 + '\n입력 형식에 맞게 입력해주세요. 아무 것도 입력하지 않으시면 기본 값이 적용됩니다.\n' + '=' * 50)
+
+    # Load default values from configs.json
+    with open('./configs.json', 'r', encoding='utf8') as f_in:
+        configs = json.load(f_in)
+        default_query = configs['default_query']
+        default_max_pages = configs['max_pages']
+        default_sort_type = configs['sort_type']
+
+        date_range = configs['date_range'] or 30
+        time_format = '%Y.%m.%d'
+        default_start_date = (datetime.now() - timedelta(days=int(date_range))).strftime(time_format)
+        default_end_date = datetime.now().strftime(time_format)
+
+        output_dir = configs['output_directory']
+        if output_dir is None:
+            output_dir = './results'
+        os.makedirs(output_dir, exist_ok=True)
+
+    # Get command line inputs
+    max_pages = input(f'최대 크롤링할 페이지 수 입력하시오 [Default={default_max_pages}]: ') or default_max_pages
+    query = input(f'검색어 입력 [Default={default_query}]: ') or default_query
+    sort_type = input(f'뉴스 검색 방식 입력 (관련도순=0, 최신순=1, 오래된순=2) [Default={default_sort_type}]: ') or default_sort_type
+    start_date = input(f'시작날짜 입력 [Default={default_start_date}]: ') or default_start_date  # 2020.01.04
+    end_date = input(f'끝날짜 입력 [Default={default_end_date}]: ') or default_end_date   # 2020.01.05
+
+    # Run crawler
+    crawler(max_pages, query, sort_type, start_date, end_date, output_dir)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
